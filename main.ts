b0cac40006f0b9ca0a8f884a53978308e8ca0b03
sergiorami123 <132910772+sergiorami123@users.noreply.github.com>
2023-05-08 07:44:10 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,67 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    basic.pause(1500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # # . .
+        # # # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # # # # .
+        # # # # .
+        # # # # .
+        # # # # .
+        # # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.pause(1500)
+    basic.clearScreen()
+    basic.pause(1500)
+    basic.showString("SERGIO RAMÍREZ STEFANI BURITICA ISAIAS GALICIA ")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.pause(1500)
+    basic.clearScreen()
+})
